b2d2225934774745b240c329e145921c83825178
Abolfazl Rahbar <abolfazlrahbarofficial@gmail.com>
2025-07-27 16:29:05 +0330

    feat: add searchBar widget in HomePage.dart

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:travel/widgets/appBar.dart';
+import 'package:travel/widgets/searchBar.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [Appbar()]));
+    return Scaffold(
+      body: Column(children: [Appbar(), SizedBox(height: 8), Searchbar()]),
+    );
   }
 }
