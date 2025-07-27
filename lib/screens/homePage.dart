ad2212f52611e61c819121857c4fd2db7f9c55d8
Abolfazl Rahbar <abolfazlrahbarofficial@gmail.com>
2025-07-27 15:47:53 +0330

    feat: add appBar in HomePage.dart

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:travel/widgets/appBar.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: Column(children: [Appbar()]));
   }
 }
