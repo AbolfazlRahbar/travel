1c05870dafaeca32f93188504721d9d545a89b4c
Abolfazl Rahbar <abolfazlrahbarofficial@gmail.com>
2025-07-30 15:35:42 +0330

    add Overviewdetailwidegt in Overviewtraveldetail

--- lib/widgets/travelDetialPageWidgets/overviewTravelDetail.dart
+++ lib/widgets/travelDetialPageWidgets/overviewTravelDetail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel/widgets/travelDetialPageWidgets/overViewDetailWidegt.dart';
 
 class Overviewtraveldetail extends StatelessWidget {
   const Overviewtraveldetail({super.key});
@@ -29,6 +30,16 @@ class Overviewtraveldetail extends StatelessWidget {
                   fontWeight: FontWeight.w600,
                 ),
               ),
+            ],
+          ),
+          SizedBox(height: 18),
+          Row(
+            children: [
+              Overviewdetailwidegt(icon: Icons.lock_clock, title: '8 hours'),
+              SizedBox(width: 48),
+              Overviewdetailwidegt(icon: Icons.cloud, title: '16 C'),
+              SizedBox(width: 48),
+              Overviewdetailwidegt(icon: Icons.star, title: '4.5'),
             ],
           ),
         ],
