a14fc873b38331db1eef6500a9d2c59991e51c4b
Abolfazl Rahbar <abolfazlrahbarofficial@gmail.com>
2025-07-31 14:16:54 +0330

    add Text to overViewDetail.dart

--- lib/widgets/travelDetialPageWidgets/overviewTravelDetail.dart
+++ lib/widgets/travelDetialPageWidgets/overviewTravelDetail.dart
@@ -6,43 +6,65 @@ class Overviewtraveldetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(left: 12),
-      height: 200,
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Text(
-                'Overview',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 22,
-                  fontWeight: FontWeight.w600,
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.only(left: 12),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  'Overview',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(width: 16),
+                Text(
+                  'Details',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 18),
+            Row(
+              children: [
+                Overviewdetailwidegt(icon: Icons.lock_clock, title: '8 hours'),
+                SizedBox(width: 48),
+                Overviewdetailwidegt(icon: Icons.cloud, title: '16 C'),
+                SizedBox(width: 48),
+                Overviewdetailwidegt(icon: Icons.star, title: '4.5'),
+              ],
+            ),
+            SizedBox(height: 26),
+            Padding(
+              padding: EdgeInsetsGeometry.symmetric(horizontal: 12),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.white, Colors.grey.shade100],
+                  ),
+                ),
+                child: Text(
+                  'This vast mountain range is renowned for its remarkable diversity in terms of topography and climate. It features towering peaks, active volcanoes, deep canyons, expansive plateaus, and lush valleys. The Andes are also home to ',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey,
+                  ),
                 ),
               ),
-              SizedBox(width: 16),
-              Text(
-                'Details',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 18),
-          Row(
-            children: [
-              Overviewdetailwidegt(icon: Icons.lock_clock, title: '8 hours'),
-              SizedBox(width: 48),
-              Overviewdetailwidegt(icon: Icons.cloud, title: '16 C'),
-              SizedBox(width: 48),
-              Overviewdetailwidegt(icon: Icons.star, title: '4.5'),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
